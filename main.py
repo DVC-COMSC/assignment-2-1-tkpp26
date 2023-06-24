5bf6427d967850ed033b5ff228c2e4b658db33d0
Shizuka <tkpp26@gmail.com>
2023-06-24 00:15:24 -0700

    Edit main.py to calculate the percentage of males and females

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    num_males = int(input("Enter the number of males: "))
+    num_females = int(input("Enter the number of females: "))
+    total = num_females + num_males
+    m_perc = num_males / total * 100
+    f_perc = num_females / total * 100
+
+    print(f"The total number of students    \t {total}")
+    print(f"The number of males and females \t {num_males} \t {num_females}")
+    print(f"The total number of students    \t {m_perc:.2f} % \t {f_perc:.2f} %40")
 
     """
     ########################################
